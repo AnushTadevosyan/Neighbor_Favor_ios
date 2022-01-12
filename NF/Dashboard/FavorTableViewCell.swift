c3e374c36c27ceabb120c8bd016a9f65a3ab1283
Anush Tadevosyan <anntade100@gmail.com>
2022-01-12 11:29:23 -0800

    Working on enabling search option for dashboard

---
+++ NF/Dashboard/FavorTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavorTableViewCell.swift
+//  NF
+//
+//  Created by Anush Tadevosyan on 1/12/22.
+//
+
+import UIKit
+
+class FavorTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ NF/Model/favorModel.swift
@@ -0,0 +1,8 @@
+//
+//  favorModel.swift
+//  NF
+//
+//  Created by Anush Tadevosyan on 1/12/22.
+//
+
+import Foundation
